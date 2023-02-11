743bf43cd305897e82f1fe33ec84116f150a8315
tabsp <tabsp@qq.com>
2023-02-11 14:59:02 +0800

    Modidy zoom shortcut

--- config.h
+++ config.h
@@ -92,7 +92,7 @@ static Key keys[] = {
         { MODKEY|Mod4Mask,              XK_o,          incrohgaps,     {.i = -1 } },
         { MODKEY|ShiftMask,             XK_y,          incrovgaps,     {.i = +1 } },
         { MODKEY|ShiftMask,             XK_o,          incrovgaps,     {.i = -1 } },
-	{ MODKEY,                       XK_Return,     zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return,     zoom,           {0} },
 	{ MODKEY,                       XK_Tab,        view,           {0} },
 	{ MODKEY,                       XK_q,          killclient,     {0} },
 	{ MODKEY,                       XK_t,          setlayout,      {.v = &layouts[0]} },
